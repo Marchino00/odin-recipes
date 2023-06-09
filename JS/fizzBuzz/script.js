945b9cd53db3a88bc4e1dd57a015f7e207439a6f
Marco Mechini <117976377+Marchino00@users.noreply.github.com>
2023-06-09 12:14:36 +0200

    FizzBuzz

---
+++ JS/fizzBuzz/script.js
@@ -0,0 +1,17 @@
+let answer = parseInt(prompt("Please enter the number you  would like to FizzBuzz up to: "))
+
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 & i % 5 === 0) {
+        console.log("FizzBuzz " + i);
+    }
+    else if(i % 3 === 0) {
+        console.log("Fizz " + i);
+    } 
+    else if (i % 5 === 0) {
+        console.log("Buzz " + i);
+    }
+    else{
+        console.log(i);
+    }
+    
+}
